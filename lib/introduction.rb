3e2fae56d3a9fc9d42e0d5fc23eb44556598041c
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-18 10:58:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,4 +2,5 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 def introduction_with_languages(name , languages)
-  putd 
+  putd "Hi, my name is #{name} and I am learning to program in #{language}."
+end
